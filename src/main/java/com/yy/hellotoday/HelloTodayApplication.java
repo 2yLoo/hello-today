4fb3de23ee471450b6183202bcc3d6d85606f763
2yLoo <yamolv@qq.com>
2019-05-11 15:46:12 +0800

    项目初始化

---
+++ src/main/java/com/yy/hellotoday/HelloTodayApplication.java
@@ -0,0 +1,16 @@
+package com.yy.hellotoday;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@EnableScheduling
+public class HelloTodayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloTodayApplication.class, args);
+    }
+
+}
